902cd1fead27be7cd1738ce794ccb2f7776f2a8f
KotHleb <kotneeshgit@gmail.com>
2016-05-15 12:54:41 +0600

    commit_1.0.8

---
+++ GameOne Client/Assets/GameInfo/Link/Container/LinkContainer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SimpleTeam.GameOne.Scene
+{
+    public class LinkInfoContainer
+    {
+        private ArrayList _infoContainer;
+
+        public ArrayList InfoContainer { get { return _infoContainer; } }
+    }
+}
---
+++ GameOne Client/Assets/GameInfo/Link/Info/SimplusLinkInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleTeam.GameOne.Scene
+{
+    public class SimplusLinkInfo
+    {
+        private byte _id;
+
+        private SimplusInfo _source;
+        private SimplusInfo _destination;
+
+        private IObj2D _current;
+        private IObj2D _lost;
+
+        private SimplusLinkActionState _state;
+
+        public SimplusInfo Source
+        {
+            get
+            {
+                return _source;
+            }
+
+            set
+            {
+                _source = value;
+            }
+        }
+
+        public byte Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public SimplusInfo Destination
+        {
+            get
+            {
+                return _destination;
+            }
+
+            set
+            {
+                _destination = value;
+            }
+        }
+
+        public IObj2D Current
+        {
+            get
+            {
+                return _current;
+            }
+
+            set
+            {
+                _current = value;
+            }
+        }
+
+        public IObj2D Lost
+        {
+            get
+            {
+                return _lost;
+            }
+
+            set
+            {
+                _lost = value;
+            }
+        }
+
+        public SimplusLinkActionState State
+        {
+            get
+            {
+                return _state;
+            }
+
+            set
+            {
+                _state = value;
+            }
+        }
+    }
+}
